9f23c121b5a5b497b9cdc59c1c7de159efa364b7
Kyle <47279470+unblinky@users.noreply.github.com>
2021-07-19 20:25:56 -0700

    Setup grid for digging.

--- Code/Maze/assets/Maze.gd
+++ Code/Maze/assets/Maze.gd
@@ -6,9 +6,14 @@ const ROOM_PS: PackedScene = preload("res://Maze/assets/Room.tscn")
 var maze_width: int = 5
 var maze_height: int = 5
 
+var rooms: Array = []
 
 
 func _ready():
-	var room: Room = ROOM_PS.instance()
-	add_child(room)
-
+	
+	for y in maze_height:
+		for x in maze_width:
+			var room: Room = ROOM_PS.instance()
+			room.translation = Vector3(x, 0, y)
+			rooms.append(room)
+			add_child(room)

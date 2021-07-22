b12118d6a47d911ca270257b585812036188a97c
Kyle <47279470+unblinky@users.noreply.github.com>
2021-07-21 20:42:54 -0700

    Started the worm.

--- Code/Maze/assets/Maze.gd
+++ Code/Maze/assets/Maze.gd
@@ -3,18 +3,20 @@ class_name Maze
 
 const ROOM_PS: PackedScene = preload("res://Maze/assets/Room.tscn")
 
-var maze_width: int = 5
-var maze_height: int = 5
+var maze_width: int = 15
+var maze_height: int = 15
 
 var rooms: Array = [] # Single arrayed approach. [y * array_width + x]
 
+var visited_rooms: Array = []
 
 func _ready():
+	randomize()
 	
 	# Spawn the grid.
 	for y in maze_height:
 		for x in maze_width:
-			var room: Room = ROOM_PS.instance()
+			var room = ROOM_PS.instance()
 			room.translation = Vector3(x, 0, y)
 			room.grid_x = x
 			room.grid_y = y
@@ -41,5 +43,33 @@ func _ready():
 			if room.grid_y + 1 < maze_height:
 				room.AddNeighbour(rooms[(room.grid_y + 1) * maze_width + room.grid_x])
 			
-			print(room.neighbours.size())
+			print(str(room.neighbours.size()))
+			
+	# Pathfinding.
+	# Starting point.
+	var start_x: int = 3
+	var start_y: int = 3
+				
+	var current_x: int = start_x
+	var current_y: int = start_y
 	
+	var current_room = rooms[current_y * maze_width + current_x]
+	
+	# Color and append.
+	for i in 26:
+		print(i)
+		if current_room:
+			current_room.ColorRoom(Color.brown)
+			visited_rooms.append(current_room)
+			current_room = current_room.GetUnvisitedNeighbour() # Can be null.
+		else:
+			current_room = visited_rooms.pop_back()
+			
+		
+	
+	
+	
+
+# Test Success.
+#	var my_room = rooms[7]
+#	my_room.ColorNeighbours(Color.blue)
--- Code/Maze/assets/Room.gd
+++ Code/Maze/assets/Room.gd
@@ -1,13 +1,32 @@
 extends MeshInstance
 class_name Room
 
+var visited: bool = false
 var grid_x: int = 0
 var grid_y: int = 0
 var neighbours: Array = [] # Room.
 
 
+
+func GetUnvisitedNeighbour() -> Room:
+	neighbours.shuffle()
+	for neighbour in neighbours:
+		if neighbour.visited == false:
+			return neighbour
+	
+	return null
+	
+
 func AddNeighbour(room: Room) -> void:
 	neighbours.append(room)
 
-func Visited():
-	pass
+
+func ColorNeighbours(color: Color) -> int:
+	for neighbour in neighbours:
+		neighbour.get_surface_material(0).set_shader_param("BaseColor", color)
+			
+	return neighbours.size()
+	
+	
+func ColorRoom(color: Color):
+	get_surface_material(0).set_shader_param("BaseColor", color)

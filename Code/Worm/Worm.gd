1e3d5c7782401e14eba63a317ee532910230252e
Kyle <47279470+unblinky@users.noreply.github.com>
2021-08-11 21:06:34 -0700

    Player controls done.

--- Code/Worm/Worm.gd
+++ Code/Worm/Worm.gd
@@ -9,18 +9,22 @@ func _physics_process(delta):
 	# Right
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		rotation.y = 90 * PI / 180.0
 
 	# Left
 	if Input.is_action_pressed("move_left"):
 		direction.x += -1
+		rotation.y = deg2rad(-90.0)
 
 	# Up
 	if Input.is_action_pressed("move_up"):
 		direction.z += -1
+		rotation.y = deg2rad(180.0)
 
 	# Down
 	if Input.is_action_pressed("move_down"):
 		direction.z += 1
+		rotation.y = 0
 
 	# HACK: Escape key.
 	if Input.is_action_just_pressed("quit_game"):
